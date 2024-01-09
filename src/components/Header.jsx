dd208797efac40ab825a8f5582f11fec667a9f06
Denis-Hish <denis.hish@gmail.com>
2024-01-09 11:29:04 +0100

    Not finished changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,40 @@
+import React, { useState } from 'react';
 import logoImg from '../img/flesh_logo.png';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 const Header = () => {
-   return (
-      <header>
-         <div className="container">
-            <div className="header-logo">
-               <img src={logoImg} alt="Logo" />
-            </div>
-            <div className="header-title">
-               <h1>Solar calculator</h1>
-            </div>
-            <div className="language-selector">
-               UA
-               {/* <div className="selected-language">
-                  <div className="arrow"></div>
-               </div>
-               <ul className="language-options">
-                  <li data-lang="ua">
-                     <img src="img/flag-ukraine.jpg" alt="ua" />
-                  </li>
-                  <li data-lang="pl">
-                     <img src="img/flag-poland.jpg" alt="pl" />
-                  </li>
-                  <li data-lang="en">
-                     <img src="img/flag-english.jpg" alt="en" />
-                  </li>
-               </ul> */}
-            </div>
-         </div>
-      </header>
-   );
+  /* Change language */
+  const [language, setLanguage] = useState('UA');
+
+  const handleChange = (event) => {
+    setLanguage(event.target.value);
+  };
+  /* ------------------------------------------- */
+
+  return (
+    <header>
+      <div className='container'>
+        <div className='header-logo'>
+          <img src={logoImg} alt='Logo' />
+        </div>
+        <div className='header-title'>
+          <h1>Solar calculator</h1>
+        </div>
+        <div className='language-selector'>
+          <FormControl fullWidth>
+            <Select value={language} onChange={handleChange}>
+              <MenuItem value={'RU'}>RU</MenuItem>
+              <MenuItem value={'UA'}>UA</MenuItem>
+              <MenuItem value={'PL'}>PL</MenuItem>
+              <MenuItem value={'EN'}>EN</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,146 @@
-import React, { useState } from 'react';
-import Settings from './Settings';
-import DeviceList from './DeviceList';
+import React from 'react';
+import { Button, TextField, InputAdornment } from '@mui/material';
+import { PlusLg, DashLg } from 'react-bootstrap-icons';
 import AnimatedBackground from './AnimatebBackground';
 
 const Main = () => {
-  const [totalDevicePower, setTotalDevicePower] = useState({});
-  const [selectVolt, setSelectVolt] = useState('');
-
   return (
     <main>
-      <Settings
-        updateTotalDevicePower={setTotalDevicePower}
-        totalDevicePower={totalDevicePower}
-        onSelectVoltChange={(value) => setSelectVolt(value)}
-      />
-      <DeviceList
-        updateTotalDevicePower={setTotalDevicePower}
-        onSelectVoltChange={(value) => setSelectVolt(value)}
-        selectVolt={selectVolt}
-      />
+      <div className='settings'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col'>
+              <TextField
+                className='select-volt custom-input-inset'
+                label='Напряжение'
+                variant='outlined'
+                type='number'
+                autoComplete='off'
+                value=''
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position='end'>вольт</InputAdornment>
+                  ),
+                }}
+              />
+              <TextField
+                className='length-daylight custom-input-inset'
+                label='Световой день'
+                variant='outlined'
+                type='number'
+                autoComplete='off'
+                value=''
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position='end'>часов</InputAdornment>
+                  ),
+                }}
+              />
+            </div>
+            <div className='col'>
+              <h6>У тебя есть:</h6>
+              <p>
+                Потребление <span>000</span> Вт/ч
+              </p>
+              <p>
+                Потребление <span>000</span> Ам/ч
+              </p>
+            </div>
+            <div className='col'>
+              <h6>Тебе нужно:</h6>
+              <p>
+                Мощность панелей <span>000</span> Вт
+              </p>
+              <p>
+                Получение энергии <span>000</span> Вт/ч
+              </p>
+              <p>
+                Зарядит аккумуляторы на <span>000</span> Ам/ч
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='device-list'>
+        <div className='container'>
+          <div className='title-table'>
+            <h6>Список устройств</h6>
+            <Button variant='outlined'>Очистить</Button>
+          </div>
+          <div className='input-group devices-input-group'>
+            <span className='index-number input-group-text'>1</span>
+            <TextField
+              id='name_device'
+              type='text'
+              label='Название устройства'
+              className='device custom-input-inset form-control'
+              value=''
+            />
+            <TextField
+              id='working_hours'
+              type='number'
+              label='Время работы'
+              className='device custom-input-inset form-control'
+              value=''
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>часов</InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              id='power_device'
+              type='number'
+              label='Мощность устройства'
+              className='device custom-input-inset form-control'
+              value=''
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>Вт</InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              id='consumption consumption-wt'
+              type='number'
+              label='Потребление'
+              className='device custom-input form-control'
+              disabled
+              readOnly
+              value=''
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>Вт/ч</InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              id='consumption consumption-am'
+              type='number'
+              label='Потребление'
+              className='device custom-input form-control'
+              disabled
+              readOnly
+              value=''
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>Ам/ч</InputAdornment>
+                ),
+              }}
+            />
+            <Button className='btn-line add' variant='outlined'>
+              <PlusLg />
+            </Button>
+            <Button
+              className='btn-line remove'
+              variant='outlined'
+              color='error'
+            >
+              <DashLg />
+            </Button>
+          </div>
+        </div>
+      </div>
       <AnimatedBackground />
     </main>
   );
